8ab921eaf2e5363f54806f0ca657e5ebb30fca25
MouhebSliti <mouheb.sliti@esprit.tn>
2024-06-18 10:24:15 +0100

    check

--- Controllers/fileHandler.js
+++ Controllers/fileHandler.js
@@ -46,10 +46,10 @@ async function getLatestMediaURLsForUser(companyName) {
         if (!user) {
             throw new Error('User not found');
         }
-        const LogoUrl = user.logo ? `https://marketplace-fmjs.onrender.com/media/${user.logo.filename}` : null;
-        const latestImage1Url = user.image1 ? `https://marketplace-fmjs.onrender.com/media/${user.image1.filename}` : null;
-        const latestImage2Url = user.image2 ? `https://marketplace-fmjs.onrender.com/media/${user.image2.filename}` : null;
-        const latestVideoUrl = user.video ? `https://marketplace-fmjs.onrender.com/media/${user.video.filename}` : null;
+        const LogoUrl = user.logo ? `https://marketplace-1-5g2u.onrender.com/media/${user.logo.filename}` : null;
+        const latestImage1Url = user.image1 ? `https://marketplace-1-5g2u.onrender.com/media/${user.image1.filename}` : null;
+        const latestImage2Url = user.image2 ? `https://marketplace-1-5g2u.onrender.com/media/${user.image2.filename}` : null;
+        const latestVideoUrl = user.video ? `https://marketplace-1-5g2u.onrender.com/media/${user.video.filename}` : null;
         return { Logo:LogoUrl, Image1:latestImage1Url, Image2:latestImage2Url, Video: latestVideoUrl };
     } catch (error) {
         console.error(error);
